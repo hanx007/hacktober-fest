a1585c586632100d8bc981c537a17ad123d21964
hanx007 <71213810+hanx007@users.noreply.github.com>
2020-10-04 11:36:28 +0530

    Create 02.js

---
+++ 02.js
@@ -0,0 +1,13 @@
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstInteger = 8;
+    const firstDecimal = 8.0;
+    const firstString = 'Welcome ';
+    const secondString = 'to my world! ';
+    console.log(firstInteger+secondInteger));
+    console.log(firstDecimal+secondDecimal));    
+    console.log(firstString+secondString);
+}
+
